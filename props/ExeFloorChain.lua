89db2e2b6af0305e3d51fdeb914505ec79cd8d4d
Dewald <Dewald@Dewald-PC>
2013-10-22 21:33:58 +0200

    Add sprite and position (x,y), filename, width and height arguments.

--- props/ExeFloorChain.lua
+++ props/ExeFloorChain.lua
@@ -17,39 +17,39 @@ setmetatable(ExeFloorChain, {
 })
 
 function ExeFloorChain.new(args)
-  local self = setmetatable({}, ExeFloorChain)
-  
-  self.name = args.name
-  
-  self.body = ExeMap.physWorld:addBody ( MOAIBox2DBody.STATIC )
-  self.body:setTransform(args.x,args.y)
-  
-	body_fixture = self.body:addChain( args.points )
-  body_fixture:setFriction(args.friction)
-  body_fixture:setFilter(ExeMap.BOX2D_WORLD,ExeMap.BOX2D_WORLD)
-  
-  --floorFixture:setFilter ( 0x02 )
-  --floorFixture:setCollisionHandler ( onCollide, MOAIBox2DArbiter.BEGIN + MOAIBox2DArbiter.END, 0x00 )
-  
-  --[[texture = MOAIGfxQuad2D.new ()
-  texture:setTexture ( 'moai.png' )
-  texture:setRect ( -0.5, -0.5, 0.5, 0.5 )
-  
-  sprite = MOAIProp2D.new ()
-  sprite:setDeck ( texture )
-  sprite.body = self.body
-  sprite:setParent ( self.body )
-  map.layer:insertProp ( sprite )]]
-  
-  self.body.entity = self
-  
-  return self
+    local self = setmetatable({}, ExeFloorChain)
+
+    self.name = args.name
+
+    self.body = ExeMap.physWorld:addBody ( MOAIBox2DBody.STATIC )
+    self.body:setTransform(args.x,args.y)
+    
+    local body_fixture = self.body:addChain( args.points )
+    body_fixture:setFriction(args.friction)
+    body_fixture:setFilter(ExeMap.BOX2D_WORLD,ExeMap.BOX2D_WORLD)
+    
+    --floorFixture:setFilter ( 0x02 )
+    --floorFixture:setCollisionHandler ( onCollide, MOAIBox2DArbiter.BEGIN + MOAIBox2DArbiter.END, 0x00 )
+    
+    local texture = MOAIGfxQuad2D.new ()
+    texture:setTexture ( args.filename )
+    texture:setRect ( -(args.width/2)/worldScale, -(args.height/2)/worldScale, (args.width/2)/worldScale, (args.height/2)/worldScale )
+    
+    self.sprite = MOAIProp2D.new ()
+    self.sprite:setDeck ( texture )
+    self.sprite.body = self.body
+    self.sprite:setParent ( self.body )
+    ExeMap.layer:insertProp ( self.sprite )
+    
+    self.body.entity = self
+    
+    return self
 end
 
 function ExeFloorChain:destroy()
   self.body:destroy()
-  --ExeMap.layer:removeProp ( self.sprite )
-  --self.sprite = nil
+  ExeMap.layer:removeProp ( self.sprite )
+  self.sprite = nil
 end
 
 return ExeFloorChain
