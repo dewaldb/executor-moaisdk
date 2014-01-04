e38c47af47bf25341a7c8b202738117429e20f52
dewaldb <admin@quasistellar.co.za>
2014-01-04 13:06:01 +0200

    add 3 additional layers

--- ExeGame.lua
+++ ExeGame.lua
@@ -21,14 +21,26 @@ function _M.init()
       _M.viewport:setSize ( width, height )
       _M.viewport:setScale ( width, height )
     end
+
+    _M.layer_background = MOAILayer2D.new ()
+    _M.layer_background:setViewport ( _M.viewport )
+    MOAISim.pushRenderPass ( _M.layer_background )
+
+    _M.layer_behind = MOAILayer2D.new ()
+    _M.layer_behind:setViewport ( _M.viewport )
+    MOAISim.pushRenderPass ( _M.layer_behind )
     
     _M.layer = MOAILayer2D.new ()
     _M.layer:setViewport ( _M.viewport )
     MOAISim.pushRenderPass ( _M.layer )
+
+    _M.layer_infront = MOAILayer2D.new ()
+    _M.layer_infront:setViewport ( _M.viewport )
+    MOAISim.pushRenderPass ( _M.layer_infront )
     
     _M.layer_debug = MOAILayer2D.new ()
     _M.layer_debug:setViewport ( _M.viewport )
-    MOAISim.pushRenderPass ( _M.layer_debug )
+    --MOAISim.pushRenderPass ( _M.layer_debug )
 
     _M.layer_gui = nil -- placeholder for the gui layer
 
